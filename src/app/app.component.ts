4ec4a06337084f933421214664e6e5f850a21f2b
alexbritto2000 <104192989+alexbritto2000@users.noreply.github.com>
2022-06-30 10:50:56 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+
+import * as Plyr from 'plyr';
+
+//var require: any
+//var Plyr = require('plyr');
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  @ViewChild('videoDiv', {read: ViewContainerRef}) videoDivComponent;
+  public player;
+
+  ngOnInit() {
+    document.addEventListener('DOMContentLoaded', () => {
+      //   (as seen below) works in such a way that as soon as you explicitly define (add) one control
+      // to the settings, ALL default controls are removed and you have to add them back in by defining those below.
+
+      // For example, let's say you just simply wanted to add 'restart' to the control bar in addition to the default.
+      // Once you specify *just* the 'restart' property below, ALL of the controls (progress bar, play, speed, etc) will be removed,
+      // meaning that you MUST specify 'play', 'progress', 'speed' and the other default controls to see them again.
+
+      const controls = [
+          'play-large', // The large play button in the center
+          'restart', // Restart playback
+          'rewind', // Rewind by the seek time (default 10 seconds)
+          'play', // Play/pause playback
+          'fast-forward', // Fast forward by the seek time (default 10 seconds)
+          'progress', // The progress bar and scrubber for playback and buffering
+          'current-time', // The current time of playback
+          'duration', // The full duration of the media
+          'mute', // Toggle mute
+          'volume', // Volume control
+          'captions', // Toggle captions
+          'settings', // Settings menu
+          'pip', // Picture-in-picture (currently Safari only)
+          'airplay', // Airplay (currently Safari only)
+          //'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+          'fullscreen' // Toggle fullscreen
+      ];
+
+      const player = Plyr.setup('.js-player', { controls });
+
+  });
+      //this.player = new Plyr('#plyrID', { captions: { active: true } });
+     //this.player = new Plyr(this.videoDivComponent.nativeElement, { captions: { active: true } });
+  }
+}
